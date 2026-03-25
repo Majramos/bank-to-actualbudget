cde2387622047ba76b41a8581c8041be4c1bf9ef
Marco Ramos <git@marcoramos.me>
2026-03-25 09:53:55 +0000

    update base classe

--- src/bank_to_actualbudget/file_io/base.py
+++ src/bank_to_actualbudget/file_io/base.py
@@ -3,19 +3,29 @@ from datetime import datetime
 from pathlib import Path
 from typing import Generic, TypeVar
 
-TFrame = TypeVar("TFrame")
+import polars as pl
+
+T = TypeVar("T")
 
 
-class FileIO(ABC, Generic[TFrame]):
+class FileIO(ABC, Generic[T]):
     @abstractmethod
-    def read(self, path: Path) -> TFrame:
+    def read(self, path: Path) -> T:
         pass
 
     @abstractmethod
-    def write(self, lf: TFrame) -> None:
+    def write(self, lf: pl.LazyFrame) -> None:
         pass
 
     def get_timestamped_path(self, path: Path, dformat: str = "%Y%m%d%H%M%S") -> Path:
         ts = datetime.now().strftime(dformat)
         file_name = path.stem
         return path.with_stem(f"{file_name}_{ts}")
+
+    def validate_path(self, path: Path, extension: str = ".csv") -> None:
+        """Centralized validation for file existence and type."""
+        if not path.exists():
+            raise FileNotFoundError(f"The file '{path}' does not exist.")
+
+        if path.suffix.lower() != extension.lower():
+            raise ValueError(f"File '{path}' is not a {extension} file.")
--- src/bank_to_actualbudget/file_io/csv.py
+++ src/bank_to_actualbudget/file_io/csv.py
@@ -3,17 +3,26 @@ from pathlib import Path
 import polars as pl
 
 from bank_to_actualbudget.file_io.base import FileIO
+from bank_to_actualbudget.log import get_logger
+
+log = get_logger(__name__)
 
 
 class CSVFileIO(FileIO[pl.LazyFrame]):
     def __init__(self) -> None:
-        self._input_path: Path | None = None
+        self._input_path: Path
 
     def read(self, path: Path) -> pl.LazyFrame:
+        self.validate_path(path, extension=".csv")
         self._input_path = path
-        self._output_path = path
+
+        log.info(f"Reading file: {path}")
         return pl.scan_csv(path)
 
     def write(self, lf: pl.LazyFrame) -> None:
-        self.df = lf.collect()
-        self.df.write_csv(self._output_path)
+        output_path = self.get_timestamped_path(self._input_path)
+
+        df = lf.collect()
+
+        log.info(f"Writing file: {output_path}")
+        df.write_csv(output_path)
